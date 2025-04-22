4dde9dcd02ed170e872294b30e96742185959d8d
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-04-22 18:58:25 +0300

    create init.lua

---
+++ .config/nvim/init.lua
@@ -0,0 +1,14 @@
+vim.cmd("colorscheme gruvbox")
+
+vim.o.nu = true
+vim.o.rnu = true
+
+vim.o.tabstop = 2                     -- number of columns occupied by a tab
+vim.o.softtabstop = 2                 -- see multiple spaces as tabstops so <BS> does the right thing
+vim.o.expandtab = true                -- converts tabs to white space
+vim.o.shiftwidth = 2                  -- width for autoindents
+
+vim.o.showmatch = true                -- show matching
+vim.o.ignorecase = true               -- case insensitive
+vim.o.hlsearch = true                 -- highlight search
+vim.o.incsearch = true                -- incremental search
