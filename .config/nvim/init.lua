0874bf541ba255656eb83e7271a429ad41fa95f3
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-04-24 02:07:22 +0300

    update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,15 +4,15 @@ vim.g.mapleader = ' '
 vim.o.nu = true
 vim.o.rnu = true
 
-vim.o.tabstop = 2                     -- number of columns occupied by a tab
-vim.o.softtabstop = 2                 -- see multiple spaces as tabstops so <BS> does the right thing
-vim.o.expandtab = true                -- converts tabs to white space
-vim.o.shiftwidth = 2                  -- width for autoindents
+vim.o.tabstop = 2       -- number of columns occupied by a tab
+vim.o.softtabstop = 2   -- see multiple spaces as tabstops so <BS> does the right thing
+vim.o.expandtab = true  -- converts tabs to white space
+vim.o.shiftwidth = 2    -- width for autoindents
 
-vim.o.showmatch = true                -- show matching
-vim.o.ignorecase = true               -- case insensitive
-vim.o.hlsearch = true                 -- highlight search
-vim.o.incsearch = true                -- incremental search
+vim.o.showmatch = true  -- show matching
+vim.o.ignorecase = true -- case insensitive
+vim.o.hlsearch = true   -- highlight search
+vim.o.incsearch = true  -- incremental search
 
 -- telescope
 local builtin = require('telescope.builtin')
@@ -34,7 +34,7 @@ require('telescope').setup {
 }
 
 -- treesitter
-require'nvim-treesitter.configs'.setup {
+require 'nvim-treesitter.configs'.setup {
   highlight = {
     enable = true,
 
