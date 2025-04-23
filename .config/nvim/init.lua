9f41bf575d58905b25118e5e6f6939375a7a6e96
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-04-24 01:14:37 +0300

    update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -20,3 +20,15 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find f
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+require('telescope').setup {
+  defaults = {
+    file_ignore_patterns = { ".git" },
+  },
+
+  pickers = {
+    find_files = {
+      hidden = true
+    },
+  },
+}
